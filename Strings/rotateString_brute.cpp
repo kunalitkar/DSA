2dfeba0d0182e94a52ce12772c375dda91d742fe
kunalitkar <kunalitkar@gmail.com>
2026-01-17 11:57:50 +0530

    Added check rotate string optimal is my own

---
+++ Strings/rotateString_brute.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string& s, string& goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            string rotated = s.substr(i) + s.substr(0, i);  
+            if (rotated == goal) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "rotation";
+    string goal = "tionrota";
+
+    if (sol.rotateString(s, goal)) {
+        cout << "true" << endl;  
+    } else {
+        cout << "false" << endl;
+    }
+
+    return 0;
+}
