e65de0d1904e26560675a26da6ed952bb8b04d4f
kunalitkar <kunalitkar@gmail.com>
2026-01-24 23:18:17 +0530

    Added no. of bit flipped to convert A to B

--- Bit_manipulation/Set_RightmostBit.cpp
+++ Bit_manipulation/Set_RightmostBit.cpp
@@ -1,15 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int setRightmostUnsetBit(int n) {
-    return n | (n + 1);
-}
+class Solution {
+public:
+    /* Function to get the minimum
+     bit flips to convert number */
+    int minBitsFlip(int start, int goal) {
+        
+        /* Variable to store bits that
+        are different in both numbers */
+        int num = start ^ goal;
+        
+        /* Variable to count 
+        number of set bits */
+        int count = 0;
+
+        for(int i = 0; i < 32; i++) {
+            /* Update count if the 
+            rightmost bit is set */
+            count += (num & 1); 
+            
+            /* Shift the number every 
+            time by 1 place */
+            num = num >> 1;
+        }
+        return count;
+    }
+};
 
 int main() {
-    int n = 10; 
-    int result = setRightmostUnsetBit(n);
-
-    cout << "Number after setting rightmost unset bit: " << result << endl;
-
+    int start = 10, goal = 7;
+    
+    /* Creating an instance of 
+    Solution class */
+    Solution sol; 
+    
+    /* Function call to get the minimum
+     bit flips to convert number */
+    int ans = sol.minBitsFlip(start, goal);
+    
+    cout << "The minimum bit flips to convert number is: " << ans;
+    
     return 0;
 }
